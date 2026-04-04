d59f37d709248efba7370e7508d88129cf0f9446
Mehak <mehakchhabra751@gmail.com>
2026-04-04 06:43:02 +0000

    [LeetCode Sync] Runtime - 71 ms (32.70%), Memory - 134.3 MB (50.44%)

---
+++ LeetCode/2573-remove-nodes-from-linked-list/solution.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNodes(ListNode head) {
+
+       Stack<ListNode> st=new Stack<>();
+        ListNode curr=head;
+        while(curr!=null)
+        {
+            while(!st.isEmpty() && st.peek().val<curr.val)
+            {
+                st.pop();
+            }
+            st.push(curr);
+            curr=curr.next;
+        }
+
+        while(!st.isEmpty())
+        {
+            ListNode top=st.pop();
+            top.next=curr;
+            curr=top;
+        }
+        return curr;
+        
+    }
+}
