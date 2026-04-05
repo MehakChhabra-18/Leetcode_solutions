31806edc415edb8e1129934a11115894c3343d77
Mehak <mehakchhabra751@gmail.com>
2026-04-05 07:01:30 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.9 MB (32.99%)

--- LeetCode/0225-implement-stack-using-queues/solution.java
+++ LeetCode/0225-implement-stack-using-queues/solution.java
@@ -1,6 +1,6 @@
 class MyStack {
 
-   Queue<Integer> q= new LinkedList<>();
+    Queue<Integer> q=new LinkedList<>();
     public MyStack() {
         
     }
@@ -10,28 +10,31 @@ class MyStack {
     }
     
     public int pop() {
+     for(int i=1;i<=q.size()-1;i++)
+     {
+        q.add(q.remove());
+     }
+     int val=q.remove();
+     return val;
         
-        for(int i=1;i<=q.size()-1;i++)
-        {
-            q.add(q.remove());
-        }
-        int val=q.remove();
-        return val;
+        
     }
     
     public int top() {
-        for(int i=1;i<=q.size()-1;i++)
-        {
-            q.add(q.remove());
-        }
-        int val=q.peek();
+       
+         for(int i=1;i<=q.size()-1;i++)
+     {
         q.add(q.remove());
-        return val;
+     }
+     int val=q.peek();
+     q.add(q.remove());
+     return val;
+        
     }
     
     public boolean empty() {
-        if(q.size()==0) return true;
-        else return false;
+        if(q.size()!=0) return false;
+        return true;
     }
 }
 
