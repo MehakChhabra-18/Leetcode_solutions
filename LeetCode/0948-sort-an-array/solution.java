eba6eeaf34e502837aa227f75f78fc1a0a855116
Mehak <mehakchhabra751@gmail.com>
2026-04-06 13:17:59 +0000

    [LeetCode Sync] Runtime - 24 ms (73.85%), Memory - 86.7 MB (19.06%)

---
+++ LeetCode/0948-sort-an-array/solution.java
@@ -0,0 +1,48 @@
+class Solution {
+    public static void divide(int[] nums,int start,int end)
+    {
+        if(start>=end)
+        {
+            return;
+        }
+        int mid=(start+end)/2;
+        divide(nums,start,mid);
+        divide(nums,mid+1,end);
+        conquer(nums,start,mid,end);
+    }
+
+    public static void conquer(int[] nums,int start,int mid,int end)
+    {
+        int i=start,j=mid+1;
+        int k=0;
+        int[] merged=new int[end-start+1];
+        while(i<=mid && j<=end)
+        {
+            if(nums[i]<=nums[j])
+            {
+                merged[k++]=nums[i++];
+            }
+            else
+            {
+                merged[k++]=nums[j++];
+            }
+        }
+
+        while(i<=mid)
+        {
+            merged[k++]=nums[i++];
+        }
+        while(j<=end)
+        {
+            merged[k++]=nums[j++];
+        }
+        for(i=0,j=start;i<merged.length;i++,j++)
+        {
+            nums[j]=merged[i];
+        }
+    }
+    public int[] sortArray(int[] nums) {
+       divide(nums,0,nums.length-1);
+       return nums;
+    }
+}
