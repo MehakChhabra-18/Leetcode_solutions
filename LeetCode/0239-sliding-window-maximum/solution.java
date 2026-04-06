afe67dd7dcdf45f44fb8d72ad68670f213958449
Mehak <mehakchhabra751@gmail.com>
2026-04-06 06:02:11 +0000

    [LeetCode Sync] Runtime - 30 ms (74.69%), Memory - 143.3 MB (90.51%)

--- LeetCode/0239-sliding-window-maximum/solution.java
+++ LeetCode/0239-sliding-window-maximum/solution.java
@@ -1,33 +1,26 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        
-        int n = nums.length;
-        int[] res = new int[n - k + 1];
-
-        Deque<Integer> dq = new LinkedList<>();
-
-        for (int i = 0; i < n; i++) {
-
-            // 1️⃣ Window ke bahar wala index hatao
-            if (!dq.isEmpty() && dq.peekFirst() <= i - k) {
+        int n=nums.length;
+        Deque<Integer> dq=new ArrayDeque<>();
+        int[] ans=new int[n-k+1];
+        int j=0;
+        for(int i=0;i<n;i++)
+        {
+            if(!dq.isEmpty() && dq.peekFirst()==i-k)
+            {
                 dq.pollFirst();
             }
 
-            // 2️⃣ Chhote elements hatao (peeche se)
-            while (!dq.isEmpty() && nums[dq.peekLast()] < nums[i]) {
+            while(!dq.isEmpty() && nums[dq.peekLast()]<=nums[i])
+            {
                 dq.pollLast();
             }
-
-            // 3️⃣ Current index add karo
             dq.addLast(i);
-
-            // 4️⃣ Window complete hone par answer lo
-            if (i >= k - 1) {
-                res[i - k + 1] = nums[dq.peekFirst()];
+            if(i>=k-1)
+            {
+                ans[j++]=nums[dq.peekFirst()];
             }
         }
-
-        return res;
-        
+        return ans;
     }
 }
