1bf7d4ed3d807d83fd82bffee32fb3a6bf8bac84
Mehak <mehakchhabra751@gmail.com>
2026-04-08 14:19:30 +0000

    [LeetCode Sync] Runtime - 5 ms (99.77%), Memory - 47 MB (98.97%)

--- LeetCode/0155-min-stack/solution.java
+++ LeetCode/0155-min-stack/solution.java
@@ -1,5 +1,4 @@
 class MinStack {
-
     Stack<Integer> st=new Stack<>();
     Stack<Integer> min=new Stack<>();
     public MinStack() {
@@ -7,42 +6,31 @@ class MinStack {
     }
     
     public void push(int val) {
-        if(st.size()==0)
-        {
-            st.push(val);
-            min.push(val);
-        }
-        else
-        {
-            st.push(val);
-            if(min.peek()<val)
-            {
-                min.push(min.peek());
-            }
-            else
-            {
-                min.push(val);
-            }
-        }
+      st.push(val);
+      if(!min.isEmpty() && min.peek()<val)
+      {
+        min.push(min.peek());
+      }
+      else
+      {
+        min.push(val);
+      }
     }
     
     public void pop() {
-
-        st.pop();
-        min.pop();
+     st.pop();
+     min.pop();
         
     }
     
     public int top() {
-
-        return st.peek();
-        
+       return st.peek();
+     
     }
     
     public int getMin() {
 
-        return min.peek();
-        
+     return min.peek();
     }
 }
 
