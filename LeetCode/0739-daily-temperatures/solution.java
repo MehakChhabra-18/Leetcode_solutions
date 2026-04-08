0f310ad4e8e4652c1c83674f4eeac4986617dd3e
Mehak <mehakchhabra751@gmail.com>
2026-04-08 14:26:40 +0000

    [LeetCode Sync] Runtime - 76 ms (33.90%), Memory - 106.7 MB (61.46%)

--- LeetCode/0739-daily-temperatures/solution.java
+++ LeetCode/0739-daily-temperatures/solution.java
@@ -1,13 +1,14 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer> st=new Stack<>();
         int n=temperatures.length;
         int[] res=new int[n];
-        Stack<Integer> st=new Stack<>();
         for(int i=n-1;i>=0;i--)
         {
             while(!st.isEmpty() && temperatures[st.peek()]<=temperatures[i])
             {
                 st.pop();
+        
             }
             if(st.isEmpty())
             {
@@ -19,7 +20,6 @@ class Solution {
             }
             st.push(i);
         }
-
         return res;
     }
 }
