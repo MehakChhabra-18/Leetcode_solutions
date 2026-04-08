e1dfbc0f3845fc8db69d4e0f157a63700e814ce9
Mehak <mehakchhabra751@gmail.com>
2026-04-08 14:48:07 +0000

    [LeetCode Sync] Runtime - 37 ms (11.95%), Memory - 53.9 MB (89.34%)

---
+++ LeetCode/0937-online-stock-span/solution.java
@@ -0,0 +1,29 @@
+class StockSpanner {
+
+     
+    Stack<Integer> prices=new Stack<>();
+    Stack<Integer> spans=new Stack<>();
+    public StockSpanner() {
+        
+    }
+    
+    public int next(int price) {
+      int span=1;
+      while(!prices.isEmpty() && prices.peek()<=price)
+      {
+           prices.pop();
+           span+=spans.pop();
+      }
+      prices.push(price);
+      spans.push(span);
+      return span;
+    }
+
+
+}
+    
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */
