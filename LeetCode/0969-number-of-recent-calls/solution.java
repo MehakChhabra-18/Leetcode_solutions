3293e7c305e235a60d9a0b9cf136af32f5572f8e
Mehak <mehakchhabra751@gmail.com>
2026-04-08 15:02:12 +0000

    [LeetCode Sync] Runtime - 23 ms (28.38%), Memory - 59.3 MB (68.76%)

--- LeetCode/0969-number-of-recent-calls/solution.java
+++ LeetCode/0969-number-of-recent-calls/solution.java
@@ -6,12 +6,12 @@ class RecentCounter {
     }
     
     public int ping(int t) {
-        q.add(t);
-        while(!q.isEmpty() && q.peek()<t-3000)
-        {
-            q.poll();
-        }
-        return q.size();
+      q.add(t);
+      while(!q.isEmpty() && q.peek()<t-3000)
+      {
+        q.poll();
+      }
+      return q.size();
     }
 }
 
