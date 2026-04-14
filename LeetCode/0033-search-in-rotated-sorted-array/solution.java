7dd54aba8846bb7e3f61a3b68c42817724d99127
Mehak <mehakchhabra751@gmail.com>
2026-04-14 07:19:38 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 44 MB (28.17%)

--- LeetCode/0033-search-in-rotated-sorted-array/solution.java
+++ LeetCode/0033-search-in-rotated-sorted-array/solution.java
@@ -1,43 +1,43 @@
 class Solution {
     public int search(int[] nums, int target) {
-       int low=0,high=nums.length-1;
-       while(low<=high)
-       {
-        int mid=(low+high)/2;
-        if(nums[mid]==target)
+        int n=nums.length;
+        int low=0,high=n-1;
+        while(low<=high)
         {
-            return mid;
-        }
-        if(nums[low]==nums[mid] && nums[mid]==nums[high])
-        {
-            low++;
-            high--;
-            continue;
-        }
-        if(nums[low]<=nums[mid]) // left half sorted
-        {
-            if(target>=nums[low] && target<nums[mid])
+            int mid=(low+high)/2;
+            if(nums[mid]==target)
             {
-                high=mid-1;
+                return mid;
+            }
+            if(nums[low]==nums[mid] && nums[mid]==nums[high])
+            {
+                low++;
+                high--;
+                continue;
+            }
+            if(nums[low]<=nums[mid])
+            {
+                if(target>=nums[low]&& target<nums[mid])
+                {
+                    high=mid-1;
+                }
+                else
+                {
+                    low=mid+1;
+                }
             }
             else
             {
-                low=mid+1;
+                if(target>nums[mid] && target<=nums[high])
+                {
+                    low=mid+1;
+                }
+                else
+                {
+                    high=mid-1;
+                }
             }
         }
-        else
-        {
-            if(target>nums[mid] && target<=nums[high])
-            {
-                low=mid+1;
-            }
-            else
-            {
-                high=mid-1;
-            }
-        }
-       }
-       return -1;
-        
+        return -1;
     }
 }
