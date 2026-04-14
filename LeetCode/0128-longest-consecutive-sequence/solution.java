fa5a18b2e7bcd76f3da3b3c389d0dbc744cce7a2
Mehak <mehakchhabra751@gmail.com>
2026-04-14 06:26:06 +0000

    [LeetCode Sync] Runtime - 21 ms (95.69%), Memory - 77.6 MB (84.13%)

---
+++ LeetCode/0128-longest-consecutive-sequence/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+     if(nums.length==0) return 0;
+     int count=1;
+     int maxLen=1;
+     Arrays.sort(nums);
+     int n=nums.length;
+     for(int i=1;i<n;i++)
+     {
+         if(nums[i]==nums[i-1]) continue;
+         else if(nums[i]==nums[i-1]+1) count++;
+         else count=1;
+
+         maxLen=Math.max(count,maxLen);
+     }
+     return maxLen;
+        
+    }
+}
