bbd537c811e8f9dffba9ab20e3b55fc36f8736f5
Mehak <mehakchhabra751@gmail.com>
2026-04-14 06:52:08 +0000

    [LeetCode Sync] Runtime - 30 ms (39.66%), Memory - 92.5 MB (15.22%)

--- LeetCode/0892-shortest-subarray-with-sum-at-least-k/solution.java
+++ LeetCode/0892-shortest-subarray-with-sum-at-least-k/solution.java
@@ -11,19 +11,17 @@ class Solution {
         int minLen=Integer.MAX_VALUE;
         for(int i=0;i<=n;i++)
         {
-            while(!dq.isEmpty() && prefix[i]-prefix[dq.peekFirst()]>=k)
-            {
-                minLen=Math.min(minLen,i-dq.pollFirst());
-            }
-            while(!dq.isEmpty() && prefix[i]<prefix[dq.peekLast()])
-            {
+              while(!dq.isEmpty() && prefix[i]-prefix[dq.peekFirst()]>=k)
+              {
+                   minLen=Math.min(minLen,i-dq.pollFirst());
+              }
+
+              while(!dq.isEmpty() && prefix[i]<=prefix[dq.peekLast()])
+              {
                 dq.pollLast();
-            }
-            dq.addLast(i);
+              }
+              dq.addLast(i);
         }
-
         return minLen==Integer.MAX_VALUE?-1:minLen;
-        
     }
-    }
-
+}
