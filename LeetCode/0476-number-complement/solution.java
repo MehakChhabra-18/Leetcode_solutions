971d040d228e9664c82ff5a4763a9e9bd8aab958
Mehak <mehakchhabra751@gmail.com>
2026-04-14 03:20:28 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.2 MB (40.44%)

--- LeetCode/0476-number-complement/solution.java
+++ LeetCode/0476-number-complement/solution.java
@@ -1,10 +1,10 @@
 class Solution {
     public int findComplement(int num) {
-       int mask=0;
-       while(mask<num)
-       {
+     int mask=0;
+     while(mask<num)
+     {
         mask=mask<<1|1;
-       }
-       return num^mask;
+     }   
+     return num^mask;
     }
 }
