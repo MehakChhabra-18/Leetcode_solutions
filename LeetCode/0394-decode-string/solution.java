62fc45be72b26b7080d3987f6e1113c887330ca8
Mehak <mehakchhabra751@gmail.com>
2026-04-14 04:08:45 +0000

    [LeetCode Sync] Runtime - 3 ms (22.34%), Memory - 42.7 MB (86.28%)

--- LeetCode/0394-decode-string/solution.java
+++ LeetCode/0394-decode-string/solution.java
@@ -1,42 +1,55 @@
 class Solution {
     public String decodeString(String s) {
-        Stack<String> str = new Stack<>();
-        Stack<Integer> number = new Stack<>();
-       
-        
-        int num = 0;
-        for (char c : s.toCharArray()) {
-            if (Character.isDigit(c)) {
-                num = num * 10 + (c - '0');
-            } else if (c == '[') {
+        Stack<String> str=new Stack<>();
+        Stack<Integer> number=new Stack<>();
+        int num=0;
+        for(char c: s.toCharArray())
+        {
+            
+            if(Character.isDigit(c))
+            {
+                num=num*10+(c-'0');
+            }
+            else if(c=='[')
+            {
                 number.push(num);
-                num = 0;
+                num=0;
                 str.push("[");
-               
-            } else if (Character.isLetter(c)) {
-                str.push(c + "");
-            } else {
-                StringBuilder temp = new StringBuilder();
-                while (!str.isEmpty() && !str.peek().equals("[")) {
-                    temp.insert(0,str.pop());
+            }
+            else if(Character.isLetter(c))
+            {
+                str.push(c+"");
+            }
+            else 
+            {
+                StringBuilder ans=new StringBuilder();
+                while(!str.isEmpty() && !str.peek().equals("["))
+                {
+                     ans.insert(0,str.pop());
                 }
                 str.pop();
-                int val = number.pop();
-               StringBuilder ans = new StringBuilder();
-                for (int i = 0; i < val; i++) {
-                    ans.append(temp);
+
+                StringBuilder temp=new StringBuilder();
+                int val=number.pop();
+                for(int i=0;i<val;i++)
+                {
+                    temp.append(ans);
                 }
-                for (char ch : ans.toString().toCharArray()) {
-                    str.push(ch + "");
+
+                for(char ch:temp.toString().toCharArray())
+                {
+                    str.push(ch+"");
                 }
             }
+           
         }
 
-        StringBuilder finl = new StringBuilder();
-        while (!str.isEmpty()) {
-            finl.insert(0, str.pop());
+        StringBuilder finalAns=new StringBuilder();
+        while(!str.isEmpty())
+        {
+            finalAns.insert(0,str.pop());
         }
-        return finl.toString();
-
+        return finalAns.toString();
+        
     }
 }
