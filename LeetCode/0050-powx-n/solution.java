fa73cae82ec45fccbec4ef8100f3d7e8dd6b12a7
Mehak <mehakchhabra751@gmail.com>
2026-04-14 07:38:39 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 48.1 MB (47.62%)

--- LeetCode/0050-powx-n/solution.java
+++ LeetCode/0050-powx-n/solution.java
@@ -1,21 +1,5 @@
 class Solution {
     public double myPow(double x, int n) {
-              double ans = 1.0;
-        long p = n;      // int overflow se bachne ke liye
-
-        if (p < 0) {
-            x = 1 / x;
-            p = -p;
-        }
-
-        while (p > 0) {
-            if (p % 2 == 1) {
-                ans *= x;
-            }
-            x *= x;
-            p /= 2;
-        }
-
-        return ans;
+     return (double)Math.pow(x,n);   
     }
 }
