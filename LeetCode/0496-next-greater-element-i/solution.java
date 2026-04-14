7a1c29af6ea472589fa02d0c2d8d883aaf48863c
Mehak <mehakchhabra751@gmail.com>
2026-04-14 03:45:29 +0000

    [LeetCode Sync] Runtime - 2 ms (99.50%), Memory - 45.6 MB (11.16%)

--- LeetCode/0496-next-greater-element-i/solution.java
+++ LeetCode/0496-next-greater-element-i/solution.java
@@ -1,29 +1,23 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-     Stack<Integer> st=new Stack<>();
-     int[] res=new int[10001];
-     for(int i=nums2.length-1;i>=0;i--)
-     {
-        while(!st.isEmpty() && st.peek()<=nums2[i])
+        Stack<Integer> st=new Stack<>();
+        int[] res=new int[10001];
+        for(int i=nums2.length-1;i>=0;i--)
         {
-            st.pop();
+             while(!st.isEmpty() && st.peek()<=nums2[i])
+             {
+                st.pop();
+             }
+             if(st.isEmpty()) res[nums2[i]]=-1;
+             else res[nums2[i]]=st.peek();
+             st.push(nums2[i]);
         }
-        if(st.isEmpty())
-        {
-            res[nums2[i]]=-1;
-        }
-        else
-        {
-            res[nums2[i]]=st.peek();
-        }
-        st.push(nums2[i]);
-     }
 
-     for(int i=0;i<nums1.length;i++)
-     {
-        nums1[i]=res[nums1[i]];
-     }
-     return nums1;
-       
+        for(int i=0;i<nums1.length;i++)
+        {
+            nums1[i]=res[nums1[i]];
+        }
+        return nums1;
+
     }
 }
