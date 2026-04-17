1121fc065e92838cfe1eb23a522b2271b678e5cc
Mehak <mehakchhabra751@gmail.com>
2026-04-17 04:20:23 +0000

    [LeetCode Sync] Runtime - 1 ms (99.95%), Memory - 46.8 MB (83.13%)

--- LeetCode/0136-single-number/solution.java
+++ LeetCode/0136-single-number/solution.java
@@ -1,11 +1,10 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int xor=0;
-        int n=nums.length;
-        for(int i=0;i<n;i++)
+        int result=0;
+        for(int i=0;i<nums.length;i++)
         {
-            xor=xor^nums[i];
+            result=result^nums[i];
         }
-        return xor;
+        return result;
     }
 }
