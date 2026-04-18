c99cd91443c304ba488e99ee8f7ec912e34ce64f
Mehak <mehakchhabra751@gmail.com>
2026-04-18 00:20:23 +0000

    [LeetCode Sync] Runtime - 2 ms (70.41%), Memory - 44.5 MB (65.49%)

--- LeetCode/0022-generate-parentheses/solution.java
+++ LeetCode/0022-generate-parentheses/solution.java
@@ -1,25 +1,25 @@
 class Solution {
-    public static void generate(int n,int open,int close,String ans,List<String> res)
+    public static void generate(int n,int open,int close,List<String> res,String ans)
     {
         if(ans.length()==2*n)
         {
             res.add(ans);
             return;
         }
-
+         
         if(open<n)
         {
-            generate(n,open+1,close,ans+"(",res);
+             generate(n,open+1,close,res,ans+"(");
         }
         if(close<open)
         {
-            generate(n,open,close+1,ans+")",res);
+            generate(n,open,close+1,res,ans+")");
         }
+
     }
     public List<String> generateParenthesis(int n) {
         List<String> res=new ArrayList<>();
-        generate(n,0,0,"",res);
+        generate(n,0,0,res,"");
         return res;
-        
     }
 }
