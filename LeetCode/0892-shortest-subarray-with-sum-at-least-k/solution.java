6e28436b1850002f2030a6dda544e1d02f38c50f
Mehak <mehakchhabra751@gmail.com>
2026-04-18 01:04:05 +0000

    [LeetCode Sync] Runtime - 27 ms (54.94%), Memory - 92.5 MB (15.22%)

--- LeetCode/0892-shortest-subarray-with-sum-at-least-k/solution.java
+++ LeetCode/0892-shortest-subarray-with-sum-at-least-k/solution.java
@@ -1,26 +1,26 @@
 class Solution {
     public int shortestSubarray(int[] nums, int k) {
+        Deque<Integer> dq=new ArrayDeque<>();
         int n=nums.length;
         long[] prefix=new long[n+1];
+        int minLen=Integer.MAX_VALUE;
         for(int i=0;i<n;i++)
         {
             prefix[i+1]=prefix[i]+nums[i];
         }
 
-        Deque<Integer> dq=new ArrayDeque<>();
-        int minLen=Integer.MAX_VALUE;
         for(int i=0;i<=n;i++)
         {
-              while(!dq.isEmpty() && prefix[i]-prefix[dq.peekFirst()]>=k)
-              {
-                   minLen=Math.min(minLen,i-dq.pollFirst());
-              }
+            while(!dq.isEmpty() && prefix[i]-prefix[dq.peekFirst()]>=k)
+            {
+                minLen=Math.min(minLen,i-dq.pollFirst());
+            }
 
-              while(!dq.isEmpty() && prefix[i]<=prefix[dq.peekLast()])
-              {
+            while(!dq.isEmpty() && prefix[i]<prefix[dq.peekLast()])
+            {
                 dq.pollLast();
-              }
-              dq.addLast(i);
+            }
+            dq.addLast(i);
         }
         return minLen==Integer.MAX_VALUE?-1:minLen;
     }
