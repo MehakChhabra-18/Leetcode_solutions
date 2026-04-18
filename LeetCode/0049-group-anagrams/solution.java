b8718243589bedaf7b6ba29a2d8b630d0e17e8ac
Mehak <mehakchhabra751@gmail.com>
2026-04-18 01:44:33 +0000

    [LeetCode Sync] Runtime - 6 ms (99.24%), Memory - 49.7 MB (44.31%)

--- LeetCode/0049-group-anagrams/solution.java
+++ LeetCode/0049-group-anagrams/solution.java
@@ -2,27 +2,21 @@ import java.util.*;
 
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
+       HashMap<String,List<String>> map=new HashMap<>();
+       for(String str:strs)
+       {
+           char[] arr=str.toCharArray();
+           Arrays.sort(arr);
+           String key=new String(arr);
+           if(!map.containsKey(key))
+           {
+              map.put(key,new ArrayList<>());
+           }
         
-        // Map banaya (key = sorted word, value = list of words)
-        HashMap<String, List<String>> map = new HashMap<>();
+              map.get(key).add(str);
+           
 
-        for (String str : strs) {
-            
-            // Step 1: sort karo
-            char[] ch = str.toCharArray();
-            Arrays.sort(ch);
-            String key = new String(ch);
-
-            // Step 2: agar key pehle se nahi hai → nayi list banao
-            if (!map.containsKey(key)) {
-                map.put(key, new ArrayList<>());
-            }
-
-            // Step 3: word add karo
-            map.get(key).add(str);
-        }
-
-        // Step 4: sirf values return karo
-        return new ArrayList<>(map.values());
+       }
+       return new ArrayList<>(map.values());
     }
 }
