29836d4918a39dab580b850180c12f1bbf1ffcb3
Mehak <mehakchhabra751@gmail.com>
2026-04-18 00:25:38 +0000

    [LeetCode Sync] Runtime - 2 ms (95.90%), Memory - 48.5 MB (70.75%)

--- LeetCode/0338-counting-bits/solution.java
+++ LeetCode/0338-counting-bits/solution.java
@@ -1,10 +1,10 @@
 class Solution {
     public int[] countBits(int n) {
-        int[] arr=new int[n+1];
-        for(int i=0;i<=n;i++)
-        {
-            arr[i]=Long.bitCount(i);
-        }
-        return arr;
+     int[] res=new int[n+1];
+     for(int i=0;i<=n;i++)
+     {
+        res[i]=Long.bitCount(i);
+     }   
+     return res;
     }
 }
