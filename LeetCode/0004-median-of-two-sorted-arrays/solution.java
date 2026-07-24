110d6de749b8da4edf75867ef7dd919842fd7bd1
Mehak <mehakchhabra751@gmail.com>
2026-07-24 15:47:38 +0000

    [LeetCode Sync] Runtime - 1 ms (100.00%), Memory - 48.9 MB (33.33%)

---
+++ LeetCode/0004-median-of-two-sorted-arrays/solution.java
@@ -0,0 +1,30 @@
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int i = 0, j = 0, k = 0;
+        int n1 = nums1.length, n2 = nums2.length;
+        int[] merged = new int[n1 + n2];
+        while (i < n1 && j < n2) {
+            if (nums1[i] <= nums2[j]) {
+                merged[k++] = nums1[i++];
+            } else {
+                merged[k++] = nums2[j++];
+            }
+        }
+
+        while (i < n1) {
+            merged[k++] = nums1[i++];
+        }
+
+        while (j < n2) {
+            merged[k++] = nums2[j++];
+        }
+
+        int len = merged.length;
+
+        if (len % 2 == 1) {
+            return merged[len / 2];
+        } else {
+            return (merged[len / 2 - 1] + merged[len / 2]) / 2.0;
+        }
+    }
+}
