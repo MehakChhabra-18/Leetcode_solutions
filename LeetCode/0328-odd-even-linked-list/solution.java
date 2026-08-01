1b8409dccc699e497a56db7ef8b087f1d0a6ae74
Mehak <mehakchhabra751@gmail.com>
2026-08-01 16:09:29 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 46.4 MB (36.65%)

---
+++ LeetCode/0328-odd-even-linked-list/solution.java
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        ListNode evenHead = null, evenTail = null;
+        ListNode oddHead = null, oddTail = null;
+        ListNode curr = head;
+        int i = 1;
+        while (curr != null) {
+            ListNode next=curr.next;
+            curr.next=null;
+            if (i % 2 == 0) {
+                if (evenHead == null) {
+                    evenHead = evenTail = curr;
+                } else {
+                    evenTail.next = curr;
+                    evenTail = curr;
+                }
+            }
+
+            else
+            {
+                if(oddHead==null)
+                {
+                    oddHead=oddTail=curr;
+                }
+                else
+                {
+                    oddTail.next=curr;
+                    oddTail=curr;
+                }
+            }
+
+            curr=next;
+            i++;
+        }
+
+        if(oddTail!=null)
+        {
+            oddTail.next=evenHead;
+        }
+        return oddHead;
+
+    }
+}
