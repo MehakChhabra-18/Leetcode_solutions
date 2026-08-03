15077f770879716515d6434ecb643e8ed1c4ba32
Mehak <mehakchhabra751@gmail.com>
2026-08-03 04:29:24 +0000

    [LeetCode Sync] Runtime - 5 ms (95.17%), Memory - 85.3 MB (83.24%)

---
+++ LeetCode/1522-stone-game-iii/solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String stoneGameIII(int[] stoneValue) {
+        int n=stoneValue.length;
+        int[] t=new int[n+1];
+        t[n]=0;
+        for(int i=n-1;i>=0;i--)
+        {
+            t[i]=stoneValue[i]-t[i+1];
+            if(i+1<n)
+            t[i]=Math.max(t[i],stoneValue[i]+stoneValue[i+1]-t[i+2]);
+            if(i+2<n)
+            t[i]=Math.max(t[i],stoneValue[i]+stoneValue[i+1]+stoneValue[i+2]-t[i+3]);
+        }
+
+        int diff=t[0];
+        if(diff>0) return "Alice";
+        else if(diff<0) return "Bob";
+        return "Tie";
+
+
+        
+    }
+}
