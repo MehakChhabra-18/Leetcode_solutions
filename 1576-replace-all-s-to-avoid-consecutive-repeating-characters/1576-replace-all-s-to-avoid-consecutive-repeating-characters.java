6a58088a4b88d42f4dade0557adb27beca900c3d
Mehak <mehakchhabra751@gmail.com>
2026-08-20 15:05:40 +0530

    Time: 1 ms (98.71%), Space: 44.1 MB (11.61%) - LeetHub

---
+++ 1576-replace-all-s-to-avoid-consecutive-repeating-characters/1576-replace-all-s-to-avoid-consecutive-repeating-characters.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String modifyString(String s) {
+        char[] arr = s.toCharArray();
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] == '?') {
+
+                for (char ch = 'a'; ch <= 'z'; ch++) {
+
+                    if (i > 0 && arr[i - 1] == ch) {
+                        continue;
+                    }
+
+                    if (i < arr.length - 1 && arr[i + 1] == ch) {
+                        continue;
+                    }
+
+                    arr[i] = ch;
+                    break;
+                }
+            }
+        }
+
+        return new String(arr);
+    }
+}
