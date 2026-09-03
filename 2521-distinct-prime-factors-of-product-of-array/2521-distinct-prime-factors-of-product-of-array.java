4811d23227e1d01c936b1b00499e9b90add38c03
Mehak <mehakchhabra751@gmail.com>
2026-09-03 20:54:42 +0530

    Time: 9 ms (93.22%), Space: 47.6 MB (44.96%) - LeetHub

---
+++ 2521-distinct-prime-factors-of-product-of-array/2521-distinct-prime-factors-of-product-of-array.java
@@ -0,0 +1,20 @@
+class Solution {
+   public int distinctPrimeFactors(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            for (int i = 2; i * i <= num; i++) {
+                if (num % i == 0) {
+                    set.add(i);
+                    while (num % i == 0) {
+                        num = num / i;
+                    }
+                }
+            }
+            if (num > 1) {
+                set.add(num);
+            }
+        }
+
+        return set.size();
+    }
+}
