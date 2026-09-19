296e766ec937f669b30ea8249aae0370a3bb1cdd
Mehak <mehakchhabra751@gmail.com>
2026-09-19 19:59:53 +0530

    Time: 25 ms (63.36%), Space: 48 MB (38.93%) - LeetHub

--- 1410-html-entity-parser/1410-html-entity-parser.java
+++ 1410-html-entity-parser/1410-html-entity-parser.java
@@ -1,36 +1,41 @@
 class Solution {
     public String entityParser(String text) {
+      
+        HashMap<String, String> map = new HashMap<>();
+
+        map.put("&quot;", "\"");
+        map.put("&apos;", "'");
+        map.put("&amp;", "&");
+        map.put("&gt;", ">");
+        map.put("&lt;", "<");
+        map.put("&frasl;", "/");
+
         StringBuilder ans = new StringBuilder();
 
         int i = 0;
 
         while (i < text.length()) {
 
-            if (text.startsWith("&quot;", i)) {
-                ans.append("\"");
-                i += 6;
-            }
-            else if (text.startsWith("&apos;", i)) {
-                ans.append("'");
-                i += 6;
-            }
-            else if (text.startsWith("&amp;", i)) {
-                ans.append("&");
-                i += 5;
-            }
-            else if (text.startsWith("&gt;", i)) {
-                ans.append(">");
-                i += 4;
-            }
-            else if (text.startsWith("&lt;", i)) {
-                ans.append("<");
-                i += 4;
-            }
-            else if (text.startsWith("&frasl;", i)) {
-                ans.append("/");
-                i += 7;
-            }
-            else {
+            if (text.charAt(i) == '&') {
+
+                boolean found = false;
+
+                for (String entity : map.keySet()) {
+
+                    if (text.startsWith(entity, i)) {
+                        ans.append(map.get(entity));
+                        i += entity.length();
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (!found) {
+                    ans.append('&');
+                    i++;
+                }
+
+            } else {
                 ans.append(text.charAt(i));
                 i++;
             }
